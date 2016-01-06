056ca892be99e81255e4928ea568b0d1f3b0d0f2
Elizabeth Hwang <elanneh@gmail.com>
2016-01-05 20:32:20 -0800

    added mobile responsive feature, tweaked css, and cleaned up code

--- server/controllers/guests.js
+++ server/controllers/guests.js
@@ -5,16 +5,13 @@ module.exports = (function() {
 	return {
 		index: function(request, response){
 			console.log("Server / Ctrl / Guest - Index")
-			// var orders = [{name:'Winners!!!!'}];
-			// console.log(orders);
-			// response.json(customers);
 			Guest.find({}, function(err, res){
 				if(err){
 					console.log(err);
 					response.json(err);
 					
 				}else{
-					// console.log(res);
+			
 					response.json(res);
 				}
 			})
@@ -35,54 +32,10 @@ module.exports = (function() {
 					console.log(err);
 					response.json({status:false});
 				}
-				else{
-	
-					// console.log(res);
-					response.json({status:true});
-			
-				
+				else{	
+					response.json({status:true});					
 				}
 			});	
-		// },
-
-		// update: function(request, response){
-		// 	console.log("Server / Ctrl / Bucket - Update", request.body.new_name);
-		// 	console.log(request.body.id);
-		// 	Bucket.findOneAndUpdate({_id:request.body.id}, {name: request.body.new_name}, function(err, record){
-		// 		if(err){
-		// 			console.log(err);
-		// 			response.json({status:false});
-		// 		}
-		// 		else{
-		// 			response.json(record);
-		// 		}
-		// 	})
-		// },
-		// create_item: function(request, response){
-		// 	console.log("Server / Ctrl / Items - Create", request.body.new_item);
-		// 	console.log(request.body.id);
-		// 	Bucket.findOneAndUpdate({_id:request.body.id}, {$addToSet: {items: request.body.new_item}}, function(err, record){
-		// 		if(err){
-		// 			console.log(err);
-		// 			response.json({status:false});
-		// 		}
-		// 		else{
-		// 			response.json(record);
-		// 		}
-		// 	})
-		// },
-		// destroy: function(request, response){
-		// 	console.log("Server / Ctrl / Buckets - Destroy");
-		// 	console.log("Destroy params id:", request.params.id);
-		// 	Bucket.remove({_id:request.params.id}, function(err){
-		// 		if(err){
-		// 			console.log(err);
-		// 			response.json({status:false});
-		// 		}
-		// 		else{
-		// 			response.json({status:true});
-		// 		}
-		// 	})
 		}
 	}
 })();
